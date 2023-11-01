7cd542f4894a37a974d8f4b619341500c8a4c562
DangKhai832 <dangkhai832@gmail.com>
2023-11-02 01:01:09 +0700

    cap nhat

--- demo/src/main/java/com/example/demo/DTO/HistoryDTO.java
+++ demo/src/main/java/com/example/demo/DTO/HistoryDTO.java
@@ -1,11 +1,14 @@
 package com.example.demo.DTO;
 
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 
 public interface HistoryDTO {
     Long getHistoryId();
     Long getColor();
     Long getResult();
-    Date getUpdatedTime();
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    LocalDateTime getUpdatedTime();
     Long getUserId();
 }
